2b544607ead8a139c28c192e4b768c4514e973e5
unknown <edgarlopezgonzalez25@gmail.com>
2019-09-06 21:20:46 -0500

    version 1

---
+++ src/main/java/com/example/demomavenprofiles/DemoMavenProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomavenprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMavenProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMavenProfilesApplication.class, args);
+	}
+
+}
